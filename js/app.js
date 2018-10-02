dfb0d9f133fdc4c9410b3737ddd60710a04e908b
Emre Pekel <emrepekel07@gmail.com>
2018-10-02 14:25:36 -0700

    Task 6 done - showCart button added

--- js/app.js
+++ js/app.js
@@ -106,5 +106,11 @@ var products = {
 var store = new Store(products);
 
 var showCart = function(cart) {
-    alert(cart);
-};
+    console.log(cart);
+    var cartString = Object.keys(cart).length > 0 ? "" : "Cart is empty"; // using Objects.keys() for reliability
+    for (var key in cart) {
+        cartString += key + " : " + cart[key] + "\n";
+    }
+    alert(cartString);
+    return cartString;
+}
