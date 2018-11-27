3f66e449fc02f095709eaa55bbf4de235cec06c9
adombow <addombowsky@shaw.ca>
2018-11-26 19:04:45 -0800

    Removed redundant render call

--- public/js/app.js
+++ public/js/app.js
@@ -132,13 +132,13 @@ Store.prototype.checkOut = function (onFinish) {
                     cart: thisStore.cart,
                     total: totalDue
                 },
-                function(response) {
+                function (response) {
                     console.log("postResponseSuccess");
                     alert("Your items were successfully checked out!");
                     thisStore.cart = {};
                     thisStore.onupdate();
                 },
-                function(error) {
+                function (error) {
                     console.log("postResponseError");
                     alert("Error: " + error);
                 });
@@ -149,115 +149,114 @@ Store.prototype.checkOut = function (onFinish) {
     });
 }
 
-Store.prototype.queryProducts = function(query, callback){
-	var self = this;
-	var queryString = Object.keys(query).reduce(function(acc, key){
-			return acc + (query[key] ? ((acc ? '&':'') + key + '=' + query[key]) : '');
-		}, '');
-	ajaxGet(this.serverUrl+"/products?"+queryString,
-		function(products){
-			Object.keys(products)
-				.forEach(function(itemName){
-					var rem = products[itemName].quantity - (self.cart[itemName] || 0);
-					if (rem >= 0){
-						self.stock[itemName].quantity = rem;
-					}
-					else {
-						self.stock[itemName].quantity = 0;
-						self.cart[itemName] = products[itemName].quantity;
-						if (self.cart[itemName] === 0) delete self.cart[itemName];
-					}
-					
-					self.stock[itemName] = Object.assign(self.stock[itemName], {
-						price: products[itemName].price,
-						label: products[itemName].label,
-						imageUrl: products[itemName].imageUrl
-					});
-				});
-			self.onUpdate();
-			callback(null, products);
-		},
-		function(error){
-			callback(error);
-		}
-	)
+Store.prototype.queryProducts = function (query, callback) {
+    var self = this;
+    var queryString = Object.keys(query).reduce(function (acc, key) {
+        return acc + (query[key] ? ((acc ? '&' : '') + key + '=' + query[key]) : '');
+    }, '');
+    ajaxGet(this.serverUrl + "/products?" + queryString,
+        function (products) {
+            Object.keys(products)
+                .forEach(function (itemName) {
+                    var rem = products[itemName].quantity - (self.cart[itemName] || 0);
+                    if (rem >= 0) {
+                        self.stock[itemName].quantity = rem;
+                    }
+                    else {
+                        self.stock[itemName].quantity = 0;
+                        self.cart[itemName] = products[itemName].quantity;
+                        if (self.cart[itemName] === 0) delete self.cart[itemName];
+                    }
+
+                    self.stock[itemName] = Object.assign(self.stock[itemName], {
+                        price: products[itemName].price,
+                        label: products[itemName].label,
+                        imageUrl: products[itemName].imageUrl
+                    });
+                });
+            self.onUpdate();
+            callback(null, products);
+        },
+        function (error) {
+            callback(error);
+        }
+    )
 }
 
-function renderMenu(container, storeInstance){
-	while (container.lastChild) container.removeChild(container.lastChild);
-	if (!container._filters) {
-		container._filters = {
-			minPrice: null,
-			maxPrice: null,
-			category: ''
-		};
-		container._refresh = function(){
-			storeInstance.queryProducts(container._filters, function(err, products){
-					if (err){
-						alert('Error occurred trying to query products');
-						console.log(err);
-					}
-					else {
-						displayed = Object.keys(products);
-						renderProductList(document.getElementById('productView'), storeInstance);
-					}
-				});
-		}
-	}
+function renderMenu(container, storeInstance) {
+    while (container.lastChild) container.removeChild(container.lastChild);
+    if (!container._filters) {
+        container._filters = {
+            minPrice: null,
+            maxPrice: null,
+            category: ''
+        };
+        container._refresh = function () {
+            storeInstance.queryProducts(container._filters, function (err, products) {
+                if (err) {
+                    alert('Error occurred trying to query products');
+                    console.log(err);
+                }
+                else {
+                    displayed = Object.keys(products);
+                    renderProductList(document.getElementById('productView'), storeInstance);
+                }
+            });
+        }
+    }
 
-	var box = document.createElement('div'); container.appendChild(box);
-		box.id = 'price-filter';
-		var input = document.createElement('input'); box.appendChild(input);
-			input.type = 'number';
-			input.value = container._filters.minPrice;
-			input.min = 0;
-			input.placeholder = 'Min Price';
-			input.addEventListener('blur', function(event){
-				container._filters.minPrice = event.target.value;
-				container._refresh();
-			});
+    var box = document.createElement('div'); container.appendChild(box);
+    box.id = 'price-filter';
+    var input = document.createElement('input'); box.appendChild(input);
+    input.type = 'number';
+    input.value = container._filters.minPrice;
+    input.min = 0;
+    input.placeholder = 'Min Price';
+    input.addEventListener('blur', function (event) {
+        container._filters.minPrice = event.target.value;
+        container._refresh();
+    });
 
-		input = document.createElement('input'); box.appendChild(input);
-			input.type = 'number';
-			input.value = container._filters.maxPrice;
-			input.min = 0;
-			input.placeholder = 'Max Price';
-			input.addEventListener('blur', function(event){
-				container._filters.maxPrice = event.target.value;
-				container._refresh();
-			});
+    input = document.createElement('input'); box.appendChild(input);
+    input.type = 'number';
+    input.value = container._filters.maxPrice;
+    input.min = 0;
+    input.placeholder = 'Max Price';
+    input.addEventListener('blur', function (event) {
+        container._filters.maxPrice = event.target.value;
+        container._refresh();
+    });
 
-	var list = document.createElement('ul'); container.appendChild(list);
-		list.id = 'menu';
-		var listItem = document.createElement('li'); list.appendChild(listItem);
-			listItem.className = 'menuItem' + (container._filters.category === '' ? ' active': '');
-			listItem.appendChild(document.createTextNode('All Items'));
-			listItem.addEventListener('click', function(event){
-				container._filters.category = '';
-				container._refresh()
-			});
-	var CATEGORIES = [ 'Clothing', 'Technology', 'Office', 'Outdoor' ];
-	for (var i in CATEGORIES){
-		var listItem = document.createElement('li'); list.appendChild(listItem);
-			listItem.className = 'menuItem' + (container._filters.category === CATEGORIES[i] ? ' active': '');
-			listItem.appendChild(document.createTextNode(CATEGORIES[i]));
-			listItem.addEventListener('click', (function(i){
-				return function(event){
-					container._filters.category = CATEGORIES[i];
-					container._refresh();
-				}
-			})(i));
-	}
+    var list = document.createElement('ul'); container.appendChild(list);
+    list.id = 'menu';
+    var listItem = document.createElement('li'); list.appendChild(listItem);
+    listItem.className = 'menuItem' + (container._filters.category === '' ? ' active' : '');
+    listItem.appendChild(document.createTextNode('All Items'));
+    listItem.addEventListener('click', function (event) {
+        container._filters.category = '';
+        container._refresh()
+    });
+    var CATEGORIES = ['Clothing', 'Technology', 'Office', 'Outdoor'];
+    for (var i in CATEGORIES) {
+        var listItem = document.createElement('li'); list.appendChild(listItem);
+        listItem.className = 'menuItem' + (container._filters.category === CATEGORIES[i] ? ' active' : '');
+        listItem.appendChild(document.createTextNode(CATEGORIES[i]));
+        listItem.addEventListener('click', (function (i) {
+            return function (event) {
+                container._filters.category = CATEGORIES[i];
+                container._refresh();
+            }
+        })(i));
+    }
 }
 
 var store = new Store("http://localhost:3000/");//https://cpen400a-bookstore.herokuapp.com");
 var displayed = [];
 
-store.syncWithServer(function(delta){
-    for(var prodIds in delta){
+store.syncWithServer(function (delta) {
+    for (var prodIds in delta) {
         displayed.push(prodIds);
     }
-    renderProductList(document.getElementById("productView"), store);
 });
 
 var showCart = function (cart) {
